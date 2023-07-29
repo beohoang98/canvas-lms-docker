2b4bc038b61128e089c446af55b93ab54b45d27d
An Hoang <beohoang98@gmail.com>
2023-07-29 10:57:40 +0700

    upgrade

--- canvas/entrypoint.sh
+++ canvas/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -eux
 
-nohup $HOME/canvas-lms/script/canvas_init start &
-
 # given folder permissions to app user
 chown -R app:app $HOME/canvas-lms/tmp
 chown -R app:app $HOME/canvas-lms/log
 
 mkdir -p $HOME/canvas-lms/public/dist/brandable_css
 chown -R app:app $HOME/canvas-lms/public/dist/brandable_css
+
+nohup $HOME/canvas-lms/script/canvas_init start &
 
 exec "$@"
